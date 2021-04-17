2879a51390c9a7228d9da0e20d5eb58096d9d745
a26510 <34621303+fernandoalzo@users.noreply.github.com>
2021-04-17 11:31:57 -0500

    plantilla html
    plantilla HTML

--- yt_download/yt_download.py
+++ yt_download/yt_download.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 yt_download = Blueprint("yt_download", __name__, template_folder='templates')
 
 @yt_download.route("/")
 def home():
-    return "hello world"
+    return render_template("home.html")
